7b07bd7f4114ffdf1d8ae54ac3c11838db197a05
Cristian Macedo <cristian.silvadk22@gmail.com>
2022-04-04 12:10:30 -0300

    feat: headless script that runs bouncer without pygame graphics

---
+++ headless.py
@@ -0,0 +1,15 @@
+from Bouncer import Bouncer 
+
+from constants import WIDTH
+from constants import HEIGHT
+
+running = True
+bouncer = Bouncer(None, WIDTH-1, HEIGHT-1, animate=False)
+
+while not bouncer.check_corner():
+    bouncer.update()
+
+print(                       \
+  'Corner bounce!' + ' | ' + \
+  'Collisions with wall: ' + str(bouncer.bounces)
+)
